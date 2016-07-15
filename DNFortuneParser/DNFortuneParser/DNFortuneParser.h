221c71d6bcb0a7bb384253e33e9506d42f6e2db6
David Nedrow <dnedrow@me.com>
2016-07-14 23:21:19 -0400

    Initial commit

---
+++ DNFortuneParser/DNFortuneParser/DNFortuneParser.h
@@ -0,0 +1,13 @@
+//
+//  DNFortuneParser.h
+//  DNFortuneParser
+//
+//  Created by David Nedrow on 7/14/16.
+//  Copyright © 2016 David Nedrow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DNFortuneParser : NSObject
+
+@end
---
+++ DNFortuneParser/DNFortuneParser/DNFortuneParser.m
@@ -0,0 +1,13 @@
+//
+//  DNFortuneParser.m
+//  DNFortuneParser
+//
+//  Created by David Nedrow on 7/14/16.
+//  Copyright © 2016 David Nedrow. All rights reserved.
+//
+
+#import "DNFortuneParser.h"
+
+@implementation DNFortuneParser
+
+@end
